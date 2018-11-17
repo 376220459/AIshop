73898d05aeb300a4655a92fc1888ec26bf29a2c1
wsh <376220459@qq.com>
2018-11-17 22:10:22 +0800

    finished home page

--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -50,24 +50,57 @@
                     <!-- <span>随便看看</span> -->
                     <div class="goods-nav" style="position:sticky;top:0">
                         <ul>
-                            <li>全部</li>
-                            <li>热销</li>
-                            <li>衣服</li>
-                            <li>电子</li>
-                            <li>其他</li>
+                            <li @click="goodsNavIfChange(0)" :style="goodsNavStyles[0]">全部</li>
+                            <li @click="goodsNavIfChange(1)" :style="goodsNavStyles[1]">热销</li>
+                            <li @click="goodsNavIfChange(2)" :style="goodsNavStyles[2]">衣服</li>
+                            <li @click="goodsNavIfChange(3)" :style="goodsNavStyles[3]">电子</li>
+                            <li @click="goodsNavIfChange(4)" :style="goodsNavStyles[4]">其他</li>
                         </ul>
                     </div>
-                    <ul class="goods-whole" v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="3">
-                        <li v-for="(item, index) in storage" :key="index">
+
+                    <ul v-if="goodsNavIf[0]" class="goods-whole">
+                        <li v-for="(item, index) in storageWhole" :key="index">
                             <div><img :src="item.img" alt="goods" width="100%"></div>
                             <div style="width:90%;"><p>{{ item.introduce }}</p></div>
                             <div><strong>￥{{ item.price }}</strong><span>{{ item.payCount }}人付款</span></div>
                         </li>
+                        <div @click="loadMore(storageWhole,goods)" class="goods-loading">点击加载更多...</div>
                     </ul>
-                </div>
-                
-                <div class="bottomLine" id="bottomLine" style="display:none;">
-                    做人，就要有底线嘛~
+
+                    <ul v-if="goodsNavIf[1]" class="goods-whole">
+                        <li v-for="(item, index) in storageHot" :key="index">
+                            <div><img :src="item.img" alt="goods" width="100%"></div>
+                            <div style="width:90%;"><p>{{ item.introduce }}</p></div>
+                            <div><strong>￥{{ item.price }}</strong><span>{{ item.payCount }}人付款</span></div>
+                        </li>
+                        <div @click="loadMore(storageHot,goodsHot)" class="goods-loading">点击加载更多...</div>
+                    </ul>
+                    
+                    <ul v-if="goodsNavIf[2]" class="goods-whole">
+                        <li v-for="(item, index) in storageClothes" :key="index">
+                            <div><img :src="item.img" alt="goods" width="100%"></div>
+                            <div style="width:90%;"><p>{{ item.introduce }}</p></div>
+                            <div><strong>￥{{ item.price }}</strong><span>{{ item.payCount }}人付款</span></div>
+                        </li>
+                        <div @click="loadMore(storageClothes,goodsClothes)" class="goods-loading">点击加载更多...</div>
+                    </ul>
+                    <ul v-if="goodsNavIf[3]" class="goods-whole">
+                        <li v-for="(item, index) in storageElectrical" :key="index">
+                            <div><img :src="item.img" alt="goods" width="100%"></div>
+                            <div style="width:90%;"><p>{{ item.introduce }}</p></div>
+                            <div><strong>￥{{ item.price }}</strong><span>{{ item.payCount }}人付款</span></div>
+                        </li>
+                        <div @click="loadMore(storageElectrical,goodsElectrical)" class="goods-loading">点击加载更多...</div>
+                    </ul>
+                    <ul v-if="goodsNavIf[4]" class="goods-whole">
+                        <li v-for="(item, index) in storageOthers" :key="index">
+                            <div><img :src="item.img" alt="goods" width="100%"></div>
+                            <div style="width:90%;"><p>{{ item.introduce }}</p></div>
+                            <div><strong>￥{{ item.price }}</strong><span>{{ item.payCount }}人付款</span></div>
+                        </li>
+                        <div @click="loadMore(storageOthers,goodsOthers)" class="goods-loading">点击加载更多...</div>
+                    </ul>
+
                 </div>
             </div>
         </div>
@@ -85,8 +118,13 @@ export default {
     },
     data(){
         return{
-            loading: '',
-            storage: [],
+            goodsNavStyle: ['color:red'],
+            goodsNavIf: [true],
+            storageWhole: [],
+            storageHot: [],
+            storageClothes: [],
+            storageElectrical: [],
+            storageOthers: [],
             hotGoods: ['长靴','打底裤','牛仔裤','电饭锅','折扣','苹果'],
             hotImgs: ['static/swipe/home/hot1.jpg','static/swipe/home/hot2.jpg','static/swipe/home/hot3.jpg'],
             recommendImgs: ['static/recommend/home/recommend1.jpg','static/recommend/home/recommend2.jpg','static/recommend/home/recommend3.jpg','static/recommend/home/recommend4.jpg','static/recommend/home/recommend3.jpg','static/recommend/home/recommend4.jpg'],
@@ -173,7 +211,193 @@ export default {
                     price: '36',
                     payCount: '9.1w'
                 },
+                {
+                    introduce: '英国进口Douxbebe电动吸奶器正品静音吸乳无痛全自动拔奶挤奶器。',
+                    img: 'static/goods/home/goods11.jpg',
+                    price: '55',
+                    payCount: '32'
+                },
+                {
+                    introduce: '狗狗衣服冬装冬天拉布拉多金毛萨摩耶边牧中型大型犬宠物狗四脚衣。',
+                    img: 'static/goods/home/goods12.jpg',
+                    price: '65',
+                    payCount: '324'
+                },
+                {
+                    introduce: 'HEENOOR 个性简约情侣杯，打造磨砂质感杯身，手感丝滑常握不累。',
+                    img: 'static/goods/home/goods13.jpg',
+                    price: '88',
+                    payCount: '1534'
+                },
+                {
+                    introduce: '谷梵妮束脚休闲裤，裤身两侧饰条纹拼接料,潮流瞩目。',
+                    img: 'static/goods/home/goods14.jpg',
+                    price: '124',
+                    payCount: '2231'
+                },
+            ],
+            goodsHot: [
+                {
+                    introduce: 'type-c数据线适用华为p9p10p20pro荣耀v8v9v10手机三星s8充电器4c原装6mix2s小米5x5s乐视1s2正品快充tpc加长',
+                    img: 'static/goods/home/goodsHot1.jpg',
+                    price: '8.9',
+                    payCount: '12.3w'
+                },
+                {
+                    introduce: '南极人男士内裤男平角裤纯棉质莫代尔全棉性感潮青年四角裤头裤衩',
+                    img: 'static/goods/home/goodsHot2.jpg',
+                    price: '58',
+                    payCount: '9.5w'
+                },
+                {
+                    introduce: '冬季韩版可爱家用棉拖鞋女厚底包跟居家室内情侣毛托鞋男保暖家居',
+                    img: 'static/goods/home/goodsHot3.jpg',
+                    price: '21.2',
+                    payCount: '10.8w'
+                },
+                {
+                    introduce: '【官方正版】夏科u盘32g高速金属刻字定制logo优盘创意移动迷你汽车车载电脑两用usb可爱学生男女正品大容量',
+                    img: 'static/goods/home/goodsHot4.jpg',
+                    price: '28',
+                    payCount: '50.9w'
+                },
+                {
+                    introduce: '橙子新鲜水果包邮当季脐橙甜橙手剥橙果冻橙10斤装批发麻阳冰糖橙',
+                    img: 'static/goods/home/goodsHot5.jpg',
+                    price: '26.8',
+                    payCount: '7.1w'
+                },
+                {
+                    introduce: '秋冬打底裤女外穿2018新款加绒保暖厚款内穿灰色螺纹棉裤紧身薄款',
+                    img: 'static/goods/home/goodsHot6.jpg',
+                    price: '48',
+                    payCount: '9.9w'
+                },
+            ],
+            goodsClothes: [
+                {
+                    introduce: '男士长袖T恤冬季新款加绒卫衣潮流打底衫加厚秋衣保暖上衣',
+                    img: 'static/goods/home/goodsClothes1.jpg',
+                    price: '88',
+                    payCount: '3331'
+                },
+                {
+                    introduce: '2018新款秋装秋冬季秋衣潮流加绒衣服打底衫卫衣男士长袖t恤',
+                    img: 'static/goods/home/goodsClothes2.jpg',
+                    price: '69',
+                    payCount: '2.5w'
+                },
+                {
+                    introduce: '卫衣男士长袖t恤秋装上衣韩版潮流加绒加厚打底衫男冬季衣服',
+                    img: 'static/goods/home/goodsClothes3.jpg',
+                    price: '81',
+                    payCount: '1.5w'
+                },
+                {
+                    introduce: '花花公子秋冬装棉衣男士外套男装 棉袄男韩版连帽冬季棉服男衣服',
+                    img: 'static/goods/home/goodsClothes4.jpg',
+                    price: '284',
+                    payCount: '846'
+                },
+                {
+                    introduce: '高腰加绒牛仔裤女2018冬季新款韩版显瘦chic复古学生紧身小脚长裤',
+                    img: 'static/goods/home/goodsClothes5.jpg',
+                    price: '75',
+                    payCount: '8.6w'
+                },
+                {
+                    introduce: '加绒加厚打底裤女外穿2018秋冬新款保暖黑色高腰显瘦小脚铅笔裤子',
+                    img: 'static/goods/home/goodsClothes6.jpg',
+                    price: '56',
+                    payCount: '942'
+                },
+            ],
+            goodsElectrical: [
+                {
+                    introduce: '华为 P20徕卡双镜头,AI智慧全面屏 ,杜比全景声',
+                    img: 'static/goods/home/goodsElectrical1.jpg',
+                    price: '3088',
+                    payCount: '1.5w'
+                },
+                {
+                    introduce: '小米 Max 3巨无霸级全面屏,AI双摄,双功放立体声',
+                    img: 'static/goods/home/goodsElectrical2.jpg',
+                    price: '1499',
+                    payCount: '4.8w'
+                },
+                {
+                    introduce: '松下 AG-UX90MC专业摄像机,远程控制,自动聚焦',
+                    img: 'static/goods/home/goodsElectrical3.jpg',
+                    price: '7821',
+                    payCount: '513'
+                },
+                {
+                    introduce: 'Xiaomi/小米 小米笔记本电脑Air 12.5吋256G轻薄便携学生游戏电脑笔记本官方正品超薄',
+                    img: 'static/goods/home/goodsElectrical4.jpg',
+                    price: '3999',
+                    payCount: '2.8w'
+                },
+                {
+                    introduce: '炫龙 毁灭者DD 2 dd2 GTX1050TI 4G独显 笔记本电脑超薄便捷学生 八代 酷睿i5-8400吃鸡游戏本商务本超级本',
+                    img: 'static/goods/home/goodsElectrical5.jpg',
+                    price: '5888',
+                    payCount: '1.3w'
+                },
+                {
+                    introduce: '电脑音响台式机笔记本家用迷你长条低音炮小音箱usb供电有源影响喇叭Golden Field/金河田 M16',
+                    img: 'static/goods/home/goodsElectrical6.jpg',
+                    price: '99',
+                    payCount: '1324'
+                },
+            ],
+            goodsOthers: [
+                {
+                    introduce: 'Easy北欧简约现代布艺床1.8米双人实木婚床可拆洗小户型布床主卧',
+                    img: 'static/goods/home/goodsOthers1.jpg',
+                    price: '2186',
+                    payCount: '18'
+                },
+                {
+                    introduce: '绿净全自动鞋套机家用客厅一次性脚套智能鞋膜机套鞋器鞋套盒新款',
+                    img: 'static/goods/home/goodsOthers2.jpg',
+                    price: '168',
+                    payCount: '125'
+                },
+                {
+                    introduce: 'weimeishi欧式艺术钟表 时尚创意静音时钟 美式现代简约客厅挂钟',
+                    img: 'static/goods/home/goodsOthers3.jpg',
+                    price: '325',
+                    payCount: '628'
+                },
+                {
+                    introduce: '黑天鹅蛋糕挚爱玫瑰牛奶巧克力',
+                    img: 'static/goods/home/goodsOthers4.jpg',
+                    price: '999',
+                    payCount: '521'
+                },
+                {
+                    introduce: '1919酒类直供 53度贵州茅台生肖酒（狗年）500ml',
+                    img: 'static/goods/home/goodsOthers5.jpg',
+                    price: '3450',
+                    payCount: '1.3w'
+                },
+                {
+                    introduce: '楚门吉他初学者女D50单板民谣吉他男旅行41寸电箱学生入门吉它D25',
+                    img: 'static/goods/home/goodsOthers6.jpg',
+                    price: '2135',
+                    payCount: '1324'
+                },
             ]
+        }
+    },
+    computed: {
+        goodsNavStyles(){
+            let styleWhole = this.goodsNavIf[0]==true?'color:red;font-weight:bold':'';
+            let styleHot = this.goodsNavIf[1]==true?'color:red;font-weight:bold':'';
+            let styleClothes = this.goodsNavIf[2]==true?'color:red;font-weight:bold':'';
+            let styleElectrical = this.goodsNavIf[3]==true?'color:red;font-weight:bold':'';
+            let styleOthers = this.goodsNavIf[4]==true?'color:red;font-weight:bold':'';
+            return [styleWhole,styleHot,styleClothes,styleElectrical,styleOthers];
         }
     },
     methods: {
@@ -184,38 +408,44 @@ export default {
                 duration: 500
             })
         },
-        loadMore(){
-            this.loading = true;
+        goodsNavIfChange(index){
+            this.goodsNavIf = [];
+            this.goodsNavIf[index] = true;
+        },
+        loadMore(storage,goods){
             this.$loading.open({
                 text: '正在加载',
                 spinnerType: 'fading-circle'
-            })
+            });
             setTimeout(() => {
-                if(this.storage.length == this.goods.length){
-                    this.$loading.close();
+                let last = storage.length-1;
+                this.$loading.close();
+                if(storage.length == goods.length){
                     this.$toast({
-                        message: '到底了，别刷新啦...',
+                        message: '到底啦，看看别的宝贝吧~',
                         duration: 1000
                     })
-                    let bottomLine = document.getElementById('bottomLine');
-                    bottomLine.style.display = 'flex';
-                }else{
-                    let last = this.storage.length-1;
-                    if(this.goods.length-this.storage.length>=6){
-                        for (let i = 1; i <= 6; i++) {
-                            this.storage.push(this.goods[i+last]);
-                        }
-                    }else{
-                        for (let i = this.storage.length; i <= this.goods.length-1; i++) {
-                            this.storage.push(this.goods[i]);
-                        }
+                }else if(goods.length-storage.length>=4){
+                    for (let i = 1; i <= 4; i++) {
+                        storage.push(goods[i+last]);
                     }
-                    this.loading = false;
-                    this.$loading.close();
+                }else{
+                    for (let i = storage.length; i <= goods.length-1; i++) {
+                        storage.push(goods[i]);
+                    }
                 }
-            }, 1500);
+            }, 500);
         }
-    }
+    },
+    mounted() {
+        for(let i = 0;i<=3;i++){
+            this.storageWhole.push(this.goods[i]);
+            this.storageHot.push(this.goodsHot[i]);
+            this.storageClothes.push(this.goodsClothes[i]);
+            this.storageElectrical.push(this.goodsElectrical[i]);
+            this.storageOthers.push(this.goodsOthers[i]);
+        }
+    },
 }
 </script>
 
@@ -224,7 +454,8 @@ export default {
         height: 100%;
         // background: url(/static/back/home.png);
         // background-size: cover;
-        background: #f1a775;
+        // background: #f1a775;
+        background: gainsboro;
         .footer{
             height: 10%;
             ul{
@@ -359,8 +590,10 @@ export default {
                         ul{
                             display: flex;
                             justify-content: space-around;
-                            background: #f1a775;
+                            // background: #f1a775;
+                            background: gainsboro;
                             border-top: 1px solid white;
+                            border-bottom: 1px solid white;
                             li{
                                 display: flex;
                                 align-items: center;
@@ -371,6 +604,10 @@ export default {
                             li:nth-child(5){
                                 border-right: 0;
                             }
+                            // li:hover{
+                            //     color: red;
+                            //     font-weight:bold;
+                            // }
                         }
                     }
                     .goods-whole{
@@ -409,14 +646,15 @@ export default {
                                 height: 2.5em;
                             }
                         }
+                        .goods-loading{
+                            background:gray;
+                            width:100%;
+                            height:50px;
+                            display:flex;
+                            justify-content:center;
+                            align-items:center;
+                        }
                     }
-                }
-                .bottomLine{
-                    display: flex;
-                    justify-content: center;
-                    border-top:1px dashed rgb(202, 194, 194);
-                    padding-top: 5px;
-                    margin-top: 5px;
                 }
             }
             
